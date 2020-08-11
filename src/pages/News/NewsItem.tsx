841ba8f63a6f62fd89de91d8cfc9398f5bfbd98d
David Paulsson <david@davidpaulsson.se>
2020-08-11 13:27:23 +0200

    Feat: conditionally open in new tab

--- src/pages/News/NewsItem.tsx
+++ src/pages/News/NewsItem.tsx
@@ -1,10 +1,18 @@
-import React from 'react';
-import { IItem } from '../../store';
+import React, { useContext } from 'react';
+import { IItem, store } from '../../store';
 import styles from './NewsItem.module.css';
 
 const NewsItem = ({ item }: { item: IItem }) => {
+  const { state } = useContext(store);
   return (
-    <a className={styles.link} href={item.url}>
+    <a
+      className={styles.link}
+      href={item.url}
+      rel="noopener noreferrer"
+      {...(state.settings.openLinksInNewTab && {
+        target: '_blank',
+      })}
+    >
       <li className={styles.item}>
         <h3 className={styles.title}>{item.title}</h3>
         <span className={styles.preamble}>{item.preamble}</span>
