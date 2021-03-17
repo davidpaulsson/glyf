b50accf8ff3480c6a01cd7f1f7cfb7325a61177e
davidpaulsson <davidpaulsson@users.noreply.github.com>
2021-03-17 20:01:24 +0000

    Deploying to gh-pages from  @ 5cd42181e1363afc71f7309f36af71b357048ce7 🚀

---
+++ precache-manifest.25ee31629937709e17c04f04742ce5f1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f156e08e7c3a57f37f6067153c2d7bd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1b2392f50980091105ca",
+    "url": "/static/css/2.9ce8b12c.chunk.css"
+  },
+  {
+    "revision": "7d4cdfe80e35a8bd5324",
+    "url": "/static/css/main.af55bf0d.chunk.css"
+  },
+  {
+    "revision": "1b2392f50980091105ca",
+    "url": "/static/js/2.3de41645.chunk.js"
+  },
+  {
+    "revision": "43fafa47fcc31ad801a24b8595dc7aed",
+    "url": "/static/js/2.3de41645.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d4cdfe80e35a8bd5324",
+    "url": "/static/js/main.dcc65b81.chunk.js"
+  },
+  {
+    "revision": "6fdee56ca79bca1a91e3",
+    "url": "/static/js/runtime-main.7d2bec4f.js"
+  }
+]);
